286b61d043d80fd8fd994c079e31daec10df4014
Javahir Majidli <cevahirvelizade@gmail.com>
2023-08-08 00:10:37 +0200

    Java Calculator updated

--- src/BasicMathOperationsTest.java
+++ src/BasicMathOperationsTest.java
@@ -14,14 +14,14 @@ public class BasicMathOperationsTest {
         Assert.assertEquals(-1, BasicMathOperations.sub(0, 1));
         Assert.assertEquals(0, BasicMathOperations.sub(-1, -1));
     }
-//    @Test
-//    public void testDiv() {
-//        Assert.assertEquals(-1, BasicMathOperations.sub(0, 1));
-//        Assert.assertEquals(0, BasicMathOperations.sub(-1, -1));
-//    }
+    @Test
+    public void testDiv() {
+        Assert.assertEquals(0, BasicMathOperations.sub(0, 1));
+        Assert.assertEquals(1, BasicMathOperations.sub(-1, -1));
+    }
     @Test
     public void testMul() {
-        Assert.assertEquals(-0, BasicMathOperations.sub(0, 1));
+        Assert.assertEquals(0, BasicMathOperations.sub(0, 1));
         Assert.assertEquals(1, BasicMathOperations.sub(-1, -1));
     }
 }
